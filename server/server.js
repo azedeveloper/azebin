ace1fd5ec6f966f3d162e06e88c97e8b44d3949d
AzEdownload <contact@aze.email>
2025-01-25 23:31:51 +0100

    rate limit

--- server/server.js
+++ server/server.js
@@ -8,8 +8,8 @@ const app = express();
 const port = 3000;
 
 const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, 
-	limit: 50,
+	windowMs: 30 * 60 * 1000, 
+	limit: 60,
 	standardHeaders: 'draft-8', 
 	legacyHeaders: false, 
     message: 'Too many requests, please try again later.',
